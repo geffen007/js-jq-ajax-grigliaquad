c3340bc76527939c861daa9f21a48abf2cc1b08d
Guido <guido.amodio@gmail.com>
2020-08-27 15:32:24 +0200

    add & remove class

--- js/script.js
+++ js/script.js
@@ -1,42 +1,42 @@
 //Generare una griglia 6x6 (36 boxes), ad ogni click parte una richiesta AJAX che prende un numero random da 1 a 9 (scegliere API opportuna).
 //Se è <= 5 il quadrato diventa giallo, se è > di 5 il quadrato diventa verde.
 
-
-
-
-
 $(document).ready(function() {
-
 
     var littleSquare = Handlebars.compile('<div class="little-square"><span></span></div>');
     for (var i = 0; i < 36; i++) {
     $('.big-square').append(littleSquare);
     }
 
-
     $('.little-square').click(function() {
 
-    thisSquare = $(this);
+        thisSquare = $(this);
 
-    $.ajax({
-      'url': 'https://flynn.boolean.careers/exercises/api/random/int',
-      'method': 'get',
-      'success': function(data) {
+        $.ajax({
+            'url': 'https://flynn.boolean.careers/exercises/api/random/int',
+            'method': 'get',
+            'success': function(data) {
 
-        var random = data.response;
+            var random = data.response;
+            thisSquare.children('span').text('');
+            thisSquare.children('span').append(random);
 
-        thisSquare.children('span').append(random);
+            if (random <= 5) {
+                thisSquare.addClass('yellow');
+                thisSquare.removeClass('green');
+                } else {
+                thisSquare.addClass('green');
+                thisSquare.removeClass('yellow');
+                }
+            },
 
-        if (random <= 5) {
-          thisSquare.addClass('yellow');
-        } else {
-          thisSquare.addClass('green');
-        }
-      },
-      'error': function() {
-        alert('Qualcosa è andato storto');
-      }
-    })
-    })
+
+
+            'error': function() {
+            alert('Qualcosa è andato storto');
+
+            }
+        });
+    });
 
 });
